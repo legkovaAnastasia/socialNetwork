fbda9c389a7c563f52299eaf039f998baa877b79
anastasia <legkova.anastasia127@gmail.com>
2021-11-22 11:30:27 +0300

    clean_fn

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,14 +3,17 @@ import styles from './users.module.css';
 import userPhoto from '../../assets/img/user.png'
 
 let Users = (props) => {
+    let getUsers = () => {
     if (props.users.length === 0) {
         
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             props.setUsers(response.data.items);
         });
     }
+    }
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(users => <div key={users.id}>
                 <span>
