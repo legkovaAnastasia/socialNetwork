18582d11d2dd6f42e5f5941cd86e4698bda174ae
anastasia <legkova.anastasia127@gmail.com>
2021-12-07 12:55:51 +0300

    authorization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import logo from './logo.svg';
 import './App.css'
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import { Route } from "react-router-dom";
 import News from './components/News/News';
@@ -9,13 +8,14 @@ import Settings from './components/Settings/Settings';
 import DialoguesContainer from './components/Dialogues/DialoguesContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 
 const App = (props) => {
   return (
       <div className='app-wrapper'>
-        <Header />
+        <HeaderContainer />
 
         <Navbar />
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return(
         <header className={s.header}>
         <img src='https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png' />
+        <div className={s.loginBlock}>
+          {props.isAuth ? props.login
+          : <NavLink to={'/login'} >login</NavLink>}
+        </div>
       </header>
     );
 }
